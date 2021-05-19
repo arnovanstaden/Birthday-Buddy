689e76f7a08177af5da4f0d22d15375fac924bcc
Arno van Staden <arnovanstaden@gmail.com>
2021-05-19 08:10:15 +0200

    update notifications & profile

--- src/components/UI/Notifications/Notifications.js
+++ src/components/UI/Notifications/Notifications.js
@@ -1,12 +1,17 @@
 import { SnackbarProvider } from 'notistack';
+import { useMediaQuery } from 'react-responsive'
+
 
 const NotificationsProvider = ({ children }) => {
+    const isMobile = useMediaQuery({
+        query: '(max-width: 768px)'
+    })
     return (
         <SnackbarProvider
             maxSnack={3}
             anchorOrigin={{
                 vertical: 'top',
-                horizontal: 'center',
+                horizontal: isMobile ? "left" : 'center',
             }}
         >
             {children}
