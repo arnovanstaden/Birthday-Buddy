3f17ad23577768e6bfb05609b75a7bb89993687f
Arno van Staden <arnovanstaden@gmail.com>
2021-05-29 10:20:19 +0200

    update check notif support FCM

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,64 +1,59 @@
 importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js');
 
-if (!('Notification' in window)) {
-    console.log("This browser does not support notifications.");
+const firebaseConfig = {
+    apiKey: "AIzaSyAPPJmu61h-lIecZyQVqDe6oHdp7Cg2KhM",
+    authDomain: "birthday-buddy-3daf2.firebaseapp.com",
+    projectId: "birthday-buddy-3daf2",
+    storageBucket: "birthday-buddy-3daf2.appspot.com",
+    messagingSenderId: "479609409443",
+    appId: "1:479609409443:web:5080efcc483b2175c69416",
+    measurementId: "G-E87KK2GQFG"
+};
+
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
 } else {
+    firebase.app(); // if already initialized, use that one
+}
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyAPPJmu61h-lIecZyQVqDe6oHdp7Cg2KhM",
-        authDomain: "birthday-buddy-3daf2.firebaseapp.com",
-        projectId: "birthday-buddy-3daf2",
-        storageBucket: "birthday-buddy-3daf2.appspot.com",
-        messagingSenderId: "479609409443",
-        appId: "1:479609409443:web:5080efcc483b2175c69416",
-        measurementId: "G-E87KK2GQFG"
-    };
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const app = firebase.app();
 
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
 
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    } else {
-        firebase.app(); // if already initialized, use that one
+messaging.onBackgroundMessage(function (payload) {
+    console.log('Received background message ', payload);
+    showNotification(payload)
+});
+
+self.addEventListener('notificationclick', function (e) {
+    console.log("here")
+    const notification = e.notification;
+    const primaryKey = notification.data.primaryKey;
+    if (primaryKey === 1) {
+        clients.openWindow(notification.data.url);
     }
+    notification.close();
+});
 
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    }
-    const app = firebase.app();
-
-    // Retrieve firebase messaging
-    const messaging = firebase.messaging();
-
-    messaging.onBackgroundMessage(function (payload) {
-        console.log('Received background message ', payload);
-        showNotification(payload)
-    });
-
-    self.addEventListener('notificationclick', function (e) {
-        console.log("here")
-        const notification = e.notification;
-        const primaryKey = notification.data.primaryKey;
-        if (primaryKey === 1) {
-            clients.openWindow(notification.data.url);
+function showNotification(payload) {
+    const notificationOptions = {
+        title: payload.data.title,
+        icon: '/images/logos/logo192-transparent.png',
+        badge: '/images/logos/logo192-transparent.png',
+        vibrate: [100, 50, 100],
+        body: payload.data.body,
+        data: {
+            time: new Date(Date.now()).toString(),
+            primaryKey: 1,
+            url: payload.data.url
         }
-        notification.close();
-    });
-
-    function showNotification(payload) {
-        const notificationOptions = {
-            title: payload.data.title,
-            icon: '/images/logos/logo192-transparent.png',
-            badge: '/images/logos/logo192-transparent.png',
-            vibrate: [100, 50, 100],
-            body: payload.data.body,
-            data: {
-                time: new Date(Date.now()).toString(),
-                primaryKey: 1,
-                url: payload.data.url
-            }
-        };
-        // notification.action ? options[actions] === notification.action : null;
-        self.registration.showNotification(notificationOptions.title, notificationOptions);
-    }
+    };
+    // notification.action ? options[actions] === notification.action : null;
+    self.registration.showNotification(notificationOptions.title, notificationOptions);
 }
