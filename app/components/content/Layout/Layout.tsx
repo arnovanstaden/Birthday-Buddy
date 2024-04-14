8bf6cc0ffe0f0ac1e1343074d834cd60f5bda37e
arnovanstaden <arnovanstaden@gmail.com>
2024-04-14 12:23:43 +0200

    hide navbar for auth routes

--- app/components/content/Layout/Layout.tsx
+++ app/components/content/Layout/Layout.tsx
@@ -1,14 +1,24 @@
+import { useLocation } from '@remix-run/react';
 import styles from './Layout.module.css';
 import MobileNavbar from './MobileNavbar/MobileNavbar';
+import classNames from 'classnames';
 
 const Layout: React.FC<{ children: React.ReactNode }> = (props) => {
+  const location = useLocation();
+  const isAuthRoute = location.pathname.startsWith('/auth');
+
+  const classes = classNames(
+    styles.Layout,
+    !isAuthRoute && styles.authenticated,
+  )
+
   return (
-    <div className={styles.Layout}>
+    <div className={classes}>
 
       <main>
         {props.children}
       </main>
-      <MobileNavbar />
+      {!isAuthRoute && <MobileNavbar />}
     </div>
   );
 };
